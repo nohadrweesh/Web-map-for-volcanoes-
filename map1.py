7f8df0e58694b46b6448b8b5462b613ab2be51a3
nohadrweesh <noha_drweesh@yahoo.com>
2017-10-15 17:27:06 +0200

    v managing control layer

--- map1.py
+++ map1.py
@@ -15,12 +15,22 @@ lat=list(data["LAT"])
 lon=list(data["LON"])
 elev=list(data["ELEV"])
 
-fg=folium.FeatureGroup(name="Volcanoes")
+fg1=folium.FeatureGroup(name="Volcanoes on elevation less than 1000 m")
+fg2=folium.FeatureGroup(name="Volcanoes on elevation less than 3000 m")
+fg3=folium.FeatureGroup(name="Volcanoes on elevation less than 4000 m")
+
 for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color_producer(el))))
+    if(el<1000):
+        fg1.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color_producer(el))))
+    elif(el<3000):
+        fg2.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color_producer(el))))
+    else:
+        fg3.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color_producer(el))))
     #fg.add_child(folium.CircleMarker(location[lt,ln],popup=str(el)+"m",fill=True,radius=6,color='grey',fill_opacity=0.7,fill_color=(color_producer(el))))
                  
 #fg.add_child(folium.GeoJson(data=open('world.json', 'r' ,encoding='utf-8-sig')))           
-map1.add_child(fg)
+map1.add_child(fg1)
+map1.add_child(fg2)
+map1.add_child(fg3)
 map1.add_child(folium.LayerControl())
 map1.save("map1.html")
