b7bce69cd05b0625d2526aa6a5e2480d2bc01235
nohadrweesh <noha_drweesh@yahoo.com>
2017-10-15 13:59:49 +0200

    put volcaones  on map

--- map1.py
+++ map1.py
@@ -1,6 +1,15 @@
 import folium
+import pandas as pd
+
 map1=folium.Map([38,-100],zoom_start=6,tiles="Mapbox Bright")
+
+data=pd.read_csv("Volcanoes.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+elev=list(data["ELEV"])
+
 fg=folium.FeatureGroup(name="my_map")
-fg.add_child(folium.Marker(location=[38,-100],popup="My car is here",icon=folium.Icon("green")))
+for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon("green")))
 map1.add_child(fg)
 map1.save("map1.html")
