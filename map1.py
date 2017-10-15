b100d370673572d057473c56647d85fb2e918eb0
nohadrweesh <noha_drweesh@yahoo.com>
2017-10-15 17:22:41 +0200

    v2

--- map1.py
+++ map1.py
@@ -1,5 +1,12 @@
 import folium
 import pandas as pd
+def color_producer(elevation):
+    if elevation<1000:
+        return "green"
+    elif elevation<3000:
+        return "orange"
+    else:
+        return 'red'
 
 map1=folium.Map([38,-100],zoom_start=6,tiles="Mapbox Bright")
 
@@ -8,8 +15,12 @@ lat=list(data["LAT"])
 lon=list(data["LON"])
 elev=list(data["ELEV"])
 
-fg=folium.FeatureGroup(name="my_map")
+fg=folium.FeatureGroup(name="Volcanoes")
 for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon("green")))
+    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color_producer(el))))
+    #fg.add_child(folium.CircleMarker(location[lt,ln],popup=str(el)+"m",fill=True,radius=6,color='grey',fill_opacity=0.7,fill_color=(color_producer(el))))
+                 
+#fg.add_child(folium.GeoJson(data=open('world.json', 'r' ,encoding='utf-8-sig')))           
 map1.add_child(fg)
+map1.add_child(folium.LayerControl())
 map1.save("map1.html")
